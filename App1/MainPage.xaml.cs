74fc859ea96e050bda0c637e6922c783aaeddb6b
VitaliLuca <vitalilucaster@gmail.com>
2019-10-12 11:40:43 +0200

    Initial Upload
    Basic Navigation and WebView

---
+++ App1/MainPage.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.ApplicationModel.Core;
+using System.Net.NetworkInformation;
+using Windows.UI.ViewManagement;
+
+// Pour plus d'informations sur le modèle d'élément Page vierge, consultez la page https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App1
+{
+    /// <summary>
+    /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            var coreTitleBar = CoreApplication.GetCurrentView().TitleBar; 
+            coreTitleBar.ExtendViewIntoTitleBar = true;
+            ApplicationViewTitleBar titleBar = Windows.UI.ViewManagement.ApplicationView.GetForCurrentView().TitleBar;
+            //Etend le contenu pour masquer la barre de titre
+            titleBar.ButtonBackgroundColor = Windows.UI.Colors.Transparent;
+            titleBar.ButtonForegroundColor = Windows.UI.Colors.White;
+            titleBar.ButtonHoverBackgroundColor = Windows.UI.Colors.DimGray;
+            titleBar.ButtonHoverForegroundColor = Windows.UI.Colors.White;
+            titleBar.ButtonInactiveBackgroundColor = Windows.UI.Colors.Transparent;
+            titleBar.ButtonInactiveForegroundColor = Windows.UI.Colors.Gray;
+            titleBar.ButtonPressedBackgroundColor = Windows.UI.Colors.Gray;
+            titleBar.ButtonPressedForegroundColor = Windows.UI.Colors.White;
+            //Paramétrage des boutons de fenêtre
+
+            
+
+        }
+
+        private void SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            NavigationViewItem item = args.SelectedItem as NavigationViewItem;
+            
+            switch (item.Tag.ToString()) //Navigation vers la page souhaitée
+            {
+                case "matchNav":
+                    contentFrame.Navigate(typeof(MatchPage));
+                    navigationView.Header = "Match";
+                    break;
+                case "peopleNav":
+                    contentFrame.Navigate(typeof(PeoplePage));
+                    navigationView.Header = "Jouteurs";
+                    break;
+                case "editNav":
+                    contentFrame.Navigate(typeof(EditPage));
+                    navigationView.Header = "Éditer";
+                    break;
+                case "webNav":
+                    contentFrame.Navigate(typeof(WebPage));
+                    navigationView.Header = "";
+                    break;
+            }
+        }
+    }
+}
