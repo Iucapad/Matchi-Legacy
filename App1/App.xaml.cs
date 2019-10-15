ab53d81b2f195e7c047b2bb7c0dddc1df791bd86
VitaliLuca <vitalilucaster@gmail.com>
2019-10-15 08:18:49 +0200

    Namespace change

--- App1/App.xaml.cs
+++ App1/App.xaml.cs
@@ -15,7 +15,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-namespace AppArt
+namespace App1
 {
     /// <summary>
     /// Fournit un comportement spécifique à l'application afin de compléter la classe Application par défaut.
--- App1/MainPage.xaml.cs
+++ App1/MainPage.xaml.cs
@@ -19,7 +19,7 @@ using Windows.UI.Xaml.Media.Imaging;
 
 // Pour plus d'informations sur le modèle d'élément Page vierge, consultez la page https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace AppArt
+namespace App1
 {
     /// <summary>
     /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
