3cf23bb434dd63e19625767b658f6d4d38715537
VitaliLuca <vitalilucaster@gmail.com>
2019-10-23 21:14:25 +0200

    Match Layout WIP

--- App1/Pages/CurrentMatchPage.xaml.cs
+++ App1/Pages/CurrentMatchPage.xaml.cs
@@ -25,6 +25,7 @@ namespace App1
         public CurrentMatchPage()
         {
             this.InitializeComponent();
+            page.Children.Remove(new_round);
         }
 
         private void show(object sender, RoutedEventArgs e)
@@ -44,21 +45,37 @@ namespace App1
         {
             if (((Frame)).ActualWidth < 750)
             {
+                ui_scroll.Margin = new Thickness(0, 60, 0, 60);
                 ui_container.Orientation = Orientation.Vertical;
-                ui_container.Margin = new Thickness(0, 60, 0, 60);
                 ui_container.HorizontalAlignment = HorizontalAlignment.Stretch;
                 ui_container.CornerRadius = new CornerRadius(0);
+                ui_container.Padding = new Thickness(30,5,30,5);
+                ui_vs.Height = 25;
+                ui_vs.CornerRadius = new CornerRadius(15);
+                ui_vs.Padding = new Thickness(2);
                 ui_leftcard.Height = 125;
+                ui_leftcard.HorizontalAlignment = HorizontalAlignment.Stretch;
+                ui_leftcard.Width = double.NaN;
                 ui_rightcard.Height = 125;
+                ui_rightcard.HorizontalAlignment = HorizontalAlignment.Stretch;
+                ui_rightcard.Width = double.NaN;
             }
             else
             {
+                ui_scroll.Margin = new Thickness(0, 100, 0, 60);
                 ui_container.Orientation = Orientation.Horizontal;
-                ui_container.Margin = new Thickness(20, 100, 20, 60);
                 ui_container.HorizontalAlignment = HorizontalAlignment.Center;
                 ui_container.CornerRadius = new CornerRadius(10);
+                ui_container.Padding = new Thickness(20);
+                ui_vs.Height = 50;
+                ui_vs.CornerRadius = new CornerRadius(25);
+                ui_vs.Padding = new Thickness(15);
                 ui_leftcard.Height = 250;
+                ui_leftcard.HorizontalAlignment = HorizontalAlignment.Center;
+                ui_leftcard.Width = 300;
                 ui_rightcard.Height = 250;
+                ui_rightcard.HorizontalAlignment = HorizontalAlignment.Center;
+                ui_rightcard.Width = 300;
             }
         }
     }
